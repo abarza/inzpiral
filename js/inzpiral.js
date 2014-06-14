c783f670711f4cbfd3d5949c42ce833bb3833679
Francisco Abarza <francisco@maskin.cl>
2014-06-14 09:45:46 -0400

    maqueta inicial

--- js/inzpiral.js
+++ js/inzpiral.js
@@ -1,3 +1,31 @@
+/* Carousel of children */
+$('.carousel').carousel({
+  interval: 5000
+});
+
+/* affix the navbar after scroll below header */
+$('#nav').affix({
+      offset: {
+        top: $('header').height()-$('#nav').height()
+      }
+});
+
+/* highlight the top nav as scrolling occurs */
+$('body').scrollspy({ target: '#nav' });
+
+/* smooth scrolling for scroll to top */
+$('.scroll-top').click(function(){
+  $('body,html').animate({scrollTop:0},1000);
+});
+
+/* smooth scrolling for nav sections */
+$('#nav .navbar-nav li>a').click(function(){
+  var link = $(this).attr('href');
+  var posi = $(link).offset().top+20;
+  $('body,html').animate({scrollTop:posi},700);
+});
+
+
 //jQuery to collapse the navbar on scroll
 
 $(window).scroll(function() {
@@ -32,6 +60,7 @@ $(function() {
 
 });
 
+// Not used today
 function muestramodal() {
     $('#demo').modal('show');
 }
